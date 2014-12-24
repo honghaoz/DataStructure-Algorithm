14b4a4cefd0ee112f281801f55d3a37247484800
Honghao Zhang <zhh358@gmail.com>
2014-12-24 11:56:50 -0500

    Updated 4.5

--- Chapter 4_Trees and Graphs/4.5.py
+++ Chapter 4_Trees and Graphs/4.5.py
@@ -5,7 +5,51 @@ from Queue import Queue
 # Implement a function to check if a binary tree is a binary search tree
 # General BST definition, left <= root < right
 
+# Another idea is to in-order traverse the tree and check the array
+
 def isBST(root):
+	minn, maxx = isBSTHelper(root)
+	if minn == False:
+		return False
+	else:
+		return True
+
+# This will return min and max value of tree rooted from root node
+def isBSTHelper(root):
+	if root == None:
+		return None, None
+	elif root.left == None and root.right == None:
+		return root.key, root.key
+	elif root.left == None:
+		rightMin, rightMax = isBSTHelper(root.right)
+		if rightMin == False:
+			return False, False
+		elif root.key >= rightMin:
+			return False, False
+		else:
+			return root.key, rightMax
+	elif root.right == None:
+		leftMin, leftMax = isBSTHelper(root.left)
+		if leftMin == False:
+			return False, False
+		elif leftMax > root.key:
+			return False, False
+		else:
+			return leftMin, root.key
+	else:
+		leftMin, leftMax = isBSTHelper(root.left)
+		if leftMin == False:
+			return False, False
+		rightMin, rightMax = isBSTHelper(root.right)
+		if rightMin == False:
+			return False, False
+		if leftMax > root.key or root.key >= rightMin:
+			return False, False
+		else:
+			return leftMin, rightMax
+
+# Below is a buggy function
+def isBST_Buggy(root):
 	if root == None:
 		return True
 	elif root.left == None and root.right == None:
@@ -46,6 +90,41 @@ def test():
 
 	a = [1,2,6,4,5]
 	root = createBSTWithMinimalHeight(a)
+	print "Passed" if not isBST(root) else "Failed"
+
+	root = TreeNode(3)
+	node2 = TreeNode(2)
+	root.left = node2
+	node2.right = TreeNode(4)
+	root.right = TreeNode(5)
+	print "Passed" if not isBST(root) else "Failed"
+
+	root = TreeNode(3)
+	node2 = TreeNode(2)
+	root.left = node2
+	node2.right = TreeNode(3)
+	root.right = TreeNode(5)
+	print "Passed" if isBST(root) else "Failed"
+
+	root = TreeNode(3)
+	root.left = TreeNode(2)
+	node5 = TreeNode(5)
+	root.right = node5
+	node5.left = TreeNode(4)
+	print "Passed" if isBST(root) else "Failed"
+
+	root = TreeNode(3)
+	root.left = TreeNode(2)
+	node5 = TreeNode(5)
+	root.right = node5
+	node5.left = TreeNode(2)
+	print "Passed" if not isBST(root) else "Failed"
+
+	root = TreeNode(3)
+	root.left = TreeNode(2)
+	node5 = TreeNode(5)
+	root.right = node5
+	node5.left = TreeNode(3)
 	print "Passed" if not isBST(root) else "Failed"
 
 test()
