ff1dd6fa3019f25ef19724906e6a343c27b3c6c3
Honghao Zhang <zhh358@gmail.com>
2014-12-24 16:51:32 -0500

    added 4.8

---
+++ Chapter 4_Trees and Graphs/4.8.py
@@ -0,0 +1,12 @@
+from BinaryTree import TreeNode
+from LinkedList_Basic import Node, LinkedList
+from Queue import Queue
+
+# You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes
+# Create an algorithm to decide if T2 is a subtree of T1.
+
+# One way is compare in-order and pre-order string
+
+# Alternative way is compart subtree
+
+
