2df8cdf32b674756b6a7d1a39fb631ea2fcddb5d
Honghao Zhang <zhh358@gmail.com>
2015-01-05 10:17:40 -0800

    Updated 9.5

--- Chapter 9_Recursion and Dynamic Programming/9.5.py
+++ Chapter 9_Recursion and Dynamic Programming/9.5.py
@@ -1,4 +1,22 @@
 # Write a method to compute all permutations of a string of unique characters
+# abc -> ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+def myPermutation(s):
+	if len(s) == 1:
+		return [s]
+	else:
+		result = []
+		lastChar = s[-1]
+		subS = s[:-1]
+		for eachString in myPermutation(subS):
+			for i in xrange(len(eachString) + 1):
+				result.append(insertString(eachString, i, lastChar))
+		return result
+
+def insertString(string, index, newString):
+	preString = string[:index]
+	postString = string[index:]
+	return preString + newString + postString
 
 def permutations(A, B = ''): 
 	assert len(A) >= 0 
@@ -11,4 +29,10 @@ def permutations(A, B = ''):
 			res.extend(permutations(A[0:i] + A[i+1:], B + A[i])) 
 		return res 
 
-print permutations('word')
+def test():
+	print sorted(permutations('abcd'))
+	print len(permutations("abcd"))
+	print sorted(myPermutation('abcd'))
+	print len(myPermutation('abcd'))
+
+test()
